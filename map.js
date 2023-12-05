0baae5619ae5589477ae4b2c87d80449a7a2b48e
tjay1760 <mcjthiongo@gmail.com>
2023-12-05 18:04:20 +0300

    Exercise

--- map.js
+++ map.js
@@ -1,9 +1,14 @@
-const meals = new Map();
+const movies = new Map();
+movies.set(2020, "Parasite");
+movies.set(2021, "Birdman");
+movies.set(2022, "12 years of a slave");
+movies.set(2023, "money heist");
+movies.set(204, "The challenge");
+movies.set(2025, "Mr robot");
+movies.set(2026, "the prestige");
+movies.set(2027, "Shadow");
+movies.set(2028, "american gods");
+movies.set(2029, "westword");
 
-meals.set(1, "🍞" );
-meals.set(2, "🍔" );
-meals.set(3, "🍎");
-meals.set(4, "🍌");
-meals.set(5, "GArbage");
-
-    
+val = movies.get(2020);
+console.log(val);
